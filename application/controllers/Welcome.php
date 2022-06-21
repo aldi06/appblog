ce348c6edfaf395c6af17653fcadcbebc03cdfb3
aldi06 <aldiansyahfathul@gmail.com>
2022-06-21 13:35:35 +0700

    Praktikum Ke 2

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,8 +18,13 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+	
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+	public function tes()
+	{
+		$this->load->view('template/backend/theme');
 	}
 }
