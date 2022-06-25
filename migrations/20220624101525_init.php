77152b1fd9f22bb3b9bca6ab252e6b24c817caf0
aldi06 <aldiansyahfathul@gmail.com>
2022-06-25 10:40:35 +0700

    menampung file migrasi dan membuat file migrasi user

---
+++ migrations/20220624101525_init.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Phoenix\Database\Element\Index;
+use Phoenix\Migration\AbstractMigration;
+
+final class Init extends AbstractMigration
+{
+    protected function up(): void
+    {
+        $this->table('user')
+            ->addColumn('id', 'integer', ['autoincrement' => true])
+            ->addColumn('username', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime', ['null' => true])
+            ->addIndex('username', Index::TYPE_UNIQUE)
+            ->create();
+
+        $this->insert('user', [
+            [
+                'username' => 'aldi',
+                'password' => '123',
+                'created_at' => date ('y-m-d'),
+            ]
+        ]);
+    }
+
+    protected function down(): void
+    {
+        $this->table('user')
+            ->drop();
+    }
+}
