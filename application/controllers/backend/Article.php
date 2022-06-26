4e1024b81c34c70d592c692cdb1bf44a94fb4903
aldi06 <aldiansyahfathul@gmail.com>
2022-06-26 12:44:14 +0700

    Menambah fitur ubah dan hapus artikel

--- application/controllers/backend/Article.php
+++ application/controllers/backend/Article.php
@@ -18,6 +18,7 @@ class Article extends MY_Controller{
             $post->title = $title;
             $post->article = $article;
             $post->save();
+            $notif = 'Title dan Article anda berhasil ditambahkan :)';
             }
         }
 
@@ -28,6 +29,38 @@ class Article extends MY_Controller{
 
         $post_list = Post::all();
 
-        view('backend.Article.list', ['post_list' => $post_list]);
+        $flashdata = $this->session->flashdata();
+
+        view('backend.Article.list', ['post_list' => $post_list, 'flashdata' => $flashdata]);
+    }
+
+    public function hapus($id){
+        $post = Post::find($id);
+        $post->delete();
+
+        $this->session->set_flashdata('notif', 'Selamat!!! Data anda berhasil dihapus >,<');
+        redirect('backend/article/list');
+    }
+
+    public function ubah($id){
+
+        $notif = '';
+
+        $post = Post::find($id);
+        if($this->input->post()){        
+            $title = $this->input->post('title');
+            $article = $this->input->post('article');
+
+            if($title == '' || $article == ''){
+                $notif = 'Title dan Article harus diisi ya :v';
+            }else{
+            $post->title = $title;
+            $post->article = $article;
+            $post->save();
+            $notif = 'Title dan Article anda berhasil diubah :)';
+            }
+
+        }
+        view('backend.Article.form', ['notif' => $notif, 'post' => $post]);
     }
 }
